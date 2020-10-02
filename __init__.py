2d54e6b438c6b312493af8651bcbba91a315bb5b
Emil Edin <emil.edin@gmail.com>
2020-10-02 19:58:51 +0200

    New branch with the project structuresd

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from interfaceBuilder import interface
+from interfaceBuilder import structure
+from interfaceBuilder import file_io
+from interfaceBuilder import inputs
+from interfaceBuilder import utils
